16cf6187dc5cc2bd5dd79285db034875a91d51d7
eaden <kmk@gmx.de>
2016-03-17 20:23:40 +0100

    My files. Just keep them in there for a short while since my laptops battery is dying and I have to switch my stuff over

---
+++ Cloud-Captain/Assets/Unused/AnimationBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationBehaviour : MonoBehaviour {
+
+	public bool shaking = false;
+	Animator animator;
+	Rigidbody rigid;
+	public float movementSpeed = 40;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+		rigid = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			shaking = !shaking;
+			animator.SetBool ("Shaking", shaking);  
+		}
+			
+	}
+
+	void FixedUpdate(){
+
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		horizontal = horizontal * Time.deltaTime * movementSpeed;
+		vertical = vertical * Time.deltaTime * movementSpeed;
+
+		gameObject.transform.Translate (-horizontal, 0, -vertical);
+		if (Input.GetKeyDown ("space"))
+			rigid.AddForce (0, 500, 0);
+	}
+}
---
+++ Cloud-Captain/Assets/Unused/SelectScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectScript : MonoBehaviour {
+
+
+	private int frameCounter = 0;
+	// Objects lives for 'frameCounterLimit' frames
+	private int frameCounterLimit = 3;
+
+	private BoxCollider box;
+	//GameObject parent;
+
+	private int building;
+	private int ship;
+	private int island;
+
+	public NewSelect newSelect;
+
+
+	// Use this for initialization
+	void Start () {
+//		box = gameObject.GetComponent<BoxCollider> ();
+		//newSelect = gameObject.GetComponentInParent<NewSelect>();
+		building = LayerMask.NameToLayer ("Buildings");
+		ship = LayerMask.NameToLayer ("Ships");
+		island = LayerMask.NameToLayer ("Islands");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(frameCounter>frameCounterLimit)
+			Destroy (gameObject);
+		frameCounter++;
+//		box.enabled = true;
+//		if (Input.GetKeyDown ("d")) {
+//			box.enabled = true;
+			//gameObject.GetComponentInParent<BoxCollider>().enabled
+
+	}
+
+	void OnTriggerStay(Collider coll){
+
+		if (coll.gameObject.layer == building) {
+			newSelect.buildingList.Add (coll.gameObject);
+			newSelect.newListEntry = true;
+		}
+		else if (coll.gameObject.layer == island) {
+			newSelect.islandList.Add (coll.gameObject);
+			newSelect.newListEntry = true;
+		}
+		else if (coll.gameObject.layer == ship) {
+			newSelect.shipList.Add (coll.gameObject);
+			newSelect.newListEntry = true;
+		}
+
+//		Debug.Log(coll.gameObject.layer);
+//		Debug.Log (newSelect);
+		Destroy (gameObject);
+
+	}
+
+}
