587ae16dd1c3fd63db69f8ae9be9f464d56abe65
eaden <kmk@gmx.de>
2016-03-18 05:18:04 +0100

    Done Highlighting. Moved my "Unused" stuff

---
+++ Cloud-Captain/Assets/Scripts/Kim/DestroyThis.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyThis : MonoBehaviour {
+
+    public void DestroyThisObject()
+    {
+        Destroy(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+
+}
