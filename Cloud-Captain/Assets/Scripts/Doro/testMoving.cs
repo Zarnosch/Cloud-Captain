a3d29b4b2c48bfe60ed88b99fcc3ae9167e64b8d
Doroloro <dodo-@web.de>
2016-03-19 07:37:21 +0100

    added testing scene for ship movement

---
+++ Cloud-Captain/Assets/Scripts/Doro/testMoving.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testMoving : MonoBehaviour {
+
+	public bool once = false;
+	public Vector3 target = new Vector3 (-30,0,-11);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!once) {
+			gameObject.GetComponent<ShipMove> ().moveShip (target);
+			once = true;
+		}
+	}
+}
