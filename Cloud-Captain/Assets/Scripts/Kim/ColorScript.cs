9c2b6c1b74ec5cb5112c94ba6c2af082619f2795
eaden <kmk@gmx.de>
2016-03-20 02:56:14 +0100

    Changed Color of Selection

--- Cloud-Captain/Assets/Scripts/Kim/ColorScript.cs
+++ Cloud-Captain/Assets/Scripts/Kim/ColorScript.cs
@@ -7,8 +7,15 @@ public class ColorScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		gameObject.GetComponent<Renderer> ().material.color = Color.blue;
-	}
+        Color boxColor = Color.green;
+        boxColor.a = 0.1f;
+        gameObject.GetComponent<Renderer>().material.color = boxColor;
+        //gameObject.GetComponent<Renderer>().material.SetFloat
+        //Debug.Log(gameObject.GetComponent<Renderer>().material.GetFloat("_Mode"));
+        //Debug.Log(Shader.Find("Transparency"));
+        //gameObject.GetComponent<Renderer>().material.SetFloat("_Mode", 2);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
