1bdebf92b8c8bc665350f477185e7a70df8d8e99
Zarnosch <janoszwei@yahoo.de>
2016-04-10 18:22:25 +0200

    a little fix, that the nexus had no gametype

--- Cloud-Captain/Assets/Scripts/GameobjectType.cs
+++ Cloud-Captain/Assets/Scripts/GameobjectType.cs
@@ -13,7 +13,6 @@ public class GameObjectType : MonoBehaviour
         PlayerManager.Instance.AddUnit(this, paidSupplyCost);
     }
 
-
     void OnDestroy()
     {
         PlayerManager.Instance.RemoveUnit(this);
